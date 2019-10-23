02250a9ec0f1e9c37054e92097ca6ec4bc87c2f1
k-lord <katielord27@gmail.com>
2019-10-23 16:50:39 -0400

    changed css styling, added fonts, added submit form for username, and updated logic with an onclick event for when a user submits a username

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,129 +1,173 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
+    var options = ["r","p","s"]
+    var playerOneWins = 0;
+    var playerOneLosses = 0;
+    var playerTwoWins = 0;
+    var playerTwoLosses = 0;
+    var ties = 0;
 
-var options = ["r","p","s"]
-var playerOneWins = 0;
-var playerOneLosses = 0;
-var playerTwoWins = 0;
-var playerTwoLosses = 0;
-var ties = 0;
+    //on-click event on submit button for user to enter a username in the form
+    $("#userNameSubmit").on("click", function(event) {
+        event.preventDefault();
+        var userName = $("#inlineFormInputGroupUsername2").val().trim();
+        console.log("username selected is: " + userName);
+        $("#usernameOne").text(userName);
+        $("#inlineFormInputGroupUsername2").val("Username");
+        $("#instructions").text("Type R, P, or S to start playing.")
+        $(".form-inline").hide();
+        $(".resultsTally").css("display","block");
+        $("#secondRow").css("margin-top","0.5em");
+        rpsGame();
 
-document.onkeyup = function () {
-    var playerOne = String.fromCharCode(event.keyCode).toLowerCase();
-    console.log(playerOne );
+    });
 
-    var computerGuess = options[Math.floor(Math.random()*options.length)];
-    console.log(computerGuess);
 
-    if (playerOne === "r" || playerOne === "p" || playerOne === "s"){
-        if((playerOne === "r") && (computerGuess === "r")){
-            ties++;
-            $("#instructions").text("Type R, P, or S to continue playing.");
-            $(".ties").text(ties);
-            $("#results").text("You tied!")
-            $("#letterPlayedOne").text("ROCK");
-            $("#imageOne").attr("src","assets/images/rock.png");
-            $("#letterPlayedTwo").text("ROCK");
-            $("#imageTwo").attr("src","assets/images/rock.png");
+    //function for the game logic when the user presses a button on the keyboard of either r,p, or s
+    function rpsGame() {
+        document.onkeyup = function () {
+        var playerOne = String.fromCharCode(event.keyCode).toLowerCase();
+        console.log("playerOne played: " + playerOne);
+
+        //function for computer to randomly choose either r,p, or s from the pre-defined options array
+        var computerGuess = options[Math.floor(Math.random()*options.length)];
+        console.log("playerTwo played: " + computerGuess);
+
+        //function to reset the game images to a default gif file and to clear the results and letterplayed divs
+        function resetImage () {
+            $("#results").empty();
+            $("#letterPlayedOne").empty();
+            $("#letterPlayedTwo").empty();
+            $(".image").attr("src","assets/images/giphyrps.gif");
         }
-        if((playerOne =="r") && (computerGuess=="s")){
-            playerOneWins++;
-            playerTwoLosses++;
-            $("#instructions").text("Type R, P, or S to continue playing.");
-            $("#playerOneWins").text(playerOneWins);
-            $("#playerTwoLosses").text(playerTwoLosses);
-            $("#results").text("You won!");
-            $("#letterPlayedOne").text("ROCK");
-            $("#imageOne").attr("src","assets/images/rock.png");
-            $("#letterPlayedTwo").text("SCISSORS");
-            $("#imageTwo").attr("src","assets/images/scissors.png");
+
+        //game logic
+        if (playerOne === "r" || playerOne === "p" || playerOne === "s"){
+
+            //if user presses r -----------------------------------------------------------------------
+            if((playerOne === "r") && (computerGuess === "r")){
+                ties++;
+                $("#instructions").text("Type R, P, or S to continue playing.");
+                $(".ties").text(ties);
+                $("#results").text("YOU TIED!")
+                $("#letterPlayedOne").text("ROCK");
+                $("#imageOne").attr("src","assets/images/rock2.png");
+                $("#letterPlayedTwo").text("ROCK");
+                $("#imageTwo").attr("src","assets/images/rock2.png");
+                setTimeout(resetImage, 5000);
+            }
+            if((playerOne =="r") && (computerGuess=="s")){
+                playerOneWins++;
+                playerTwoLosses++;
+                $("#instructions").text("Type R, P, or S to continue playing.");
+                $("#playerOneWins").text(playerOneWins);
+                $("#playerTwoLosses").text(playerTwoLosses);
+                $("#results").text("YOU WON!");
+                $("#letterPlayedOne").text("ROCK");
+                $("#imageOne").attr("src","assets/images/rock2.png");
+                $("#letterPlayedTwo").text("SCISSORS");
+                $("#imageTwo").attr("src","assets/images/scissor2.jpg");
+                setTimeout(resetImage, 5000);
+            }
+            if((playerOne =="r") && (computerGuess=="p")){
+                playerOneLosses++;
+                playerTwoWins++;
+                $("#instructions").text("Type R, P, or S to continue playing.");
+                $("#playerOneLosses").text(playerOneLosses);
+                $("#playerTwoWins").text(playerTwoWins);
+                $("#results").text("YOU LOST!");
+                $("#letterPlayedOne").text("ROCK");
+                $("#imageOne").attr("src","assets/images/rock2.png");
+                $("#letterPlayedTwo").text("PAPER");
+                $("#imageTwo").attr("src","assets/images/paper2.jpg");
+                setTimeout(resetImage, 5000);
+            }
+            //if user presses s -----------------------------------------------------------------------
+            if((playerOne === "s") && (computerGuess === "s")){
+                ties++;
+                $("#instructions").text("Type R, P, or S to continue playing.");
+                $(".ties").text(ties);
+                $("#results").text("YOU TIED!")
+                $("#letterPlayedOne").text("SCISSORS");
+                $("#imageOne").attr("src","assets/images/scissor2.jpg");
+                $("#letterPlayedTwo").text("SCISSORS");
+                $("#imageTwo").attr("src","assets/images/scissor2.jpg");
+                setTimeout(resetImage, 5000);
+            }
+            if((playerOne =="s") && (computerGuess=="p")){
+                playerOneWins++;
+                playerTwoLosses++;
+                $("#instructions").text("Type R, P, or S to continue playing.");
+                $("#playerOneWins").text(playerOneWins);
+                $("#playerTwoLosses").text(playerTwoLosses);
+                $("#results").text("YOU WON!");
+                $("#letterPlayedOne").text("SCISSORS");
+                $("#imageOne").attr("src","assets/images/scissor2.jpg");
+                $("#letterPlayedTwo").text("PAPER");
+                $("#imageTwo").attr("src","assets/images/paper2.jpg");
+                setTimeout(resetImage, 5000);
+            }
+            if((playerOne =="s") && (computerGuess=="r")){
+                playerOneLosses++;
+                playerTwoWins++;
+                $("#instructions").text("Type R, P, or S to continue playing.");
+                $("#playerOneLosses").text(playerOneLosses);
+                $("#playerTwoWins").text(playerTwoWins);
+                $("#results").text("YOU LOST!");
+                $("#letterPlayedOne").text("SCISSORS");
+                $("#imageOne").attr("src","assets/images/scissor2.jpg");
+                $("#letterPlayedTwo").text("ROCK");
+                $("#imageTwo").attr("src","assets/images/rock2.png");
+                setTimeout(resetImage, 5000);
+            }
+            //if user presses p -----------------------------------------------------------------------
+            if((playerOne === "p") && (computerGuess === "p")){
+                ties++;
+                $("#instructions").text("Type R, P, or S to continue playing.");
+                $(".ties").text(ties);
+                $("#results").text("YOU TIED!")
+                $("#letterPlayedOne").text("PAPER");
+                $("#imageOne").attr("src","assets/images/paper2.jpg");
+                $("#letterPlayedTwo").text("PAPER");
+                $("#imageTwo").attr("src","assets/images/paper2.jpg");
+                setTimeout(resetImage, 5000);
+            }
+            if((playerOne =="p") && (computerGuess=="r")){
+                playerOneWins++;
+                playerTwoLosses++;
+                $("#instructions").text("Type R, P, or S to continue playing.");
+                $("#playerOneWins").text(playerOneWins);
+                $("#playerTwoLosses").text(playerTwoLosses);
+                $("#results").text("YOU WON!");
+                $("#letterPlayedOne").text("PAPER");
+                $("#imageOne").attr("src","assets/images/paper2.jpg");
+                $("#letterPlayedTwo").text("ROCK");
+                $("#imageTwo").attr("src","assets/images/rock2.png");
+                setTimeout(resetImage, 5000);
+            }
+            if((playerOne =="p") && (computerGuess=="s")){
+                playerOneLosses++;
+                playerTwoWins++;
+                $("#instructions").text("Type R, P, or S to continue playing.");
+                $("#playerOneLosses").text(playerOneLosses);
+                $("#playerTwoWins").text(playerTwoWins);
+                $("#results").text("YOU LOST!");
+                $("#letterPlayedOne").text("PAPER");
+                $("#imageOne").attr("src","assets/images/paper2.jpg");
+                $("#letterPlayedTwo").text("SCISSORS");
+                $("#imageTwo").attr("src","assets/images/scissor2.jpg");
+                setTimeout(resetImage, 5000);
+            }
+        //ignore if user hits the enter button to fix bug if user hits enter instead of clicks submit button in initial start of game
+        } else if ((event.keyCode == 13)) {
+            $("#instructions").text("Type R, P, or S to start playing.");
+
+        //message shown if user hits any other button besides r,p,s, or enter
+        } else {
+            $("#instructions").text("That's not a move... Type R, P, or S.");
+            resetImage();
         }
-        if((playerOne =="r") && (computerGuess=="p")){
-            playerOneLosses++;
-            playerTwoWins++;
-            $("#instructions").text("Type R, P, or S to continue playing.");
-            $("#playerOneLosses").text(playerOneLosses);
-            $("#playerTwoWins").text(playerTwoWins);
-            $("#results").text("You lost!");
-            $("#letterPlayedOne").text("ROCK");
-            $("#imageOne").attr("src","assets/images/rock.png");
-            $("#letterPlayedTwo").text("PAPER");
-            $("#imageTwo").attr("src","assets/images/paper.png");
-        }
-        //-----------------------------------------------------------------------
-        if((playerOne === "s") && (computerGuess === "s")){
-            ties++;
-            $("#instructions").text("Type R, P, or S to continue playing.");
-            $(".ties").text(ties);
-            $("#results").text("You tied!")
-            $("#letterPlayedOne").text("SCISSORS");
-            $("#imageOne").attr("src","assets/images/scissors.png");
-            $("#letterPlayedTwo").text("SCISSORS");
-            $("#imageTwo").attr("src","assets/images/scissors.png");
-        }
-        if((playerOne =="s") && (computerGuess=="p")){
-            playerOneWins++;
-            playerTwoLosses++;
-            $("#instructions").text("Type R, P, or S to continue playing.");
-            $("#playerOneWins").text(playerOneWins);
-            $("#playerTwoLosses").text(playerTwoLosses);
-            $("#results").text("You won!");
-            $("#letterPlayedOne").text("SCISSORS");
-            $("#imageOne").attr("src","assets/images/scissors.png");
-            $("#letterPlayedTwo").text("PAPER");
-            $("#imageTwo").attr("src","assets/images/paper.png");
-        }
-        if((playerOne =="s") && (computerGuess=="r")){
-            playerOneLosses++;
-            playerTwoWins++;
-            $("#instructions").text("Type R, P, or S to continue playing.");
-            $("#playerOneLosses").text(playerOneLosses);
-            $("#playerTwoWins").text(playerTwoWins);
-            $("#results").text("You lost!");
-            $("#letterPlayedOne").text("SCISSORS");
-            $("#imageOne").attr("src","assets/images/scissors.png");
-            $("#letterPlayedTwo").text("ROCK");
-            $("#imageTwo").attr("src","assets/images/rock.png");
-        }
-        //-----------------------------------------------------------------------
-        if((playerOne === "p") && (computerGuess === "p")){
-            ties++;
-            $("#instructions").text("Type R, P, or S to continue playing.");
-            $(".ties").text(ties);
-            $("#results").text("You tied!")
-            $("#letterPlayedOne").text("PAPER");
-            $("#imageOne").attr("src","assets/images/paper.png");
-            $("#letterPlayedTwo").text("PAPER");
-            $("#imageTwo").attr("src","assets/images/paper.png");
-        }
-        if((playerOne =="p") && (computerGuess=="r")){
-            playerOneWins++;
-            playerTwoLosses++;
-            $("#instructions").text("Type R, P, or S to continue playing.");
-            $("#playerOneWins").text(playerOneWins);
-            $("#playerTwoLosses").text(playerTwoLosses);
-            $("#results").text("You won!");
-            $("#letterPlayedOne").text("PAPER");
-            $("#imageOne").attr("src","assets/images/paper.png");
-            $("#letterPlayedTwo").text("ROCK");
-            $("#imageTwo").attr("src","assets/images/rock.png");
-        }
-        if((playerOne =="p") && (computerGuess=="s")){
-            playerOneLosses++;
-            playerTwoWins++;
-            $("#instructions").text("Type R, P, or S to continue playing.");
-            $("#playerOneLosses").text(playerOneLosses);
-            $("#playerTwoWins").text(playerTwoWins);
-            $("#results").text("You lost!");
-            $("#letterPlayedOne").text("PAPER");
-            $("#imageOne").attr("src","assets/images/paper.png");
-            $("#letterPlayedTwo").text("SCISSORS");
-            $("#imageTwo").attr("src","assets/images/scissors.png");
-        }
-       
-    } else {
-        $("#instructions").text("Let's try that again... Type R, P, or S to continue playing.");
     }
 }
 
